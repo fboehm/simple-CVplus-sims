77d2e70811c8a4106caf02e8cf156243d7a08eae
Frederick J. Boehm <frederick.boehm@gmail.com>
2023-03-20 17:00:44 -0400

    fixed python code to use same Xtest & Ytest for all sample sizes

--- submit.bash
+++ submit.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
 #
-#SBATCH --job-name=jk+
-#SBATCH --output=out.txt
-#SBATCH --partition=debug
+#SBATCH --job-name=jkplus
+#SBATCH --partition=mulan,main
 #SBATCH --time=5:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
+
+source ~/miniconda3/etc/profile.d/conda.sh
+# https://github.com/conda/conda/issues/7980
 
 conda activate barber1
 quarto render barber-JK+.qmd

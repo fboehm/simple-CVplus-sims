17df617908abe0ac57df68296dcf128584c84534
Frederick J. Boehm <frederick.boehm@gmail.com>
2023-04-25 16:16:27 -0400

    feat: added lines for err & out files from slurm

--- code/submit-simple2.bash
+++ code/submit-simple2.bash
@@ -6,7 +6,8 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=64G
-
+#SBATCH --output=/net/mulan/home/fredboe/research/simple-CVplus-sims/cluster_outputs/cvp.out
+#SBATCH --error=/net/mulan/home/fredboe/research/simple-CVplus-sims/cluster_outputs/cvp.err
 
 conda run -n barber1 quarto render simple2.qmd --to html
 

bee35b8fb1fa8d0c8cd94f4f79dbcd5abc6de88c
Frederick J. Boehm <frederick.boehm@gmail.com>
2023-04-25 11:16:28 -0400

    fix: added two more sample sizes to analysis

--- code/submit-simple2.bash
+++ code/submit-simple2.bash
@@ -2,10 +2,10 @@
 #
 #SBATCH --job-name=jkplus
 #SBATCH --partition=mulan,main
-#SBATCH --time=1:00:00
+#SBATCH --time=10:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 
-quarto render simple2.qmd --to html,gfm
+quarto render simple2.qmd --to html
 
 

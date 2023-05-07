b354477bbe64c5ebc4a0dfcdacc2a7436e6bf253
Frederick J. Boehm <frederick.boehm@gmail.com>
2023-05-07 10:21:28 -0400

    feat: added R code to save var calcs

--- code/submit-simple2.bash
+++ code/submit-simple2.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-#SBATCH --job-name=jkplus
+#SBATCH --job-name=cvplus
 #SBATCH --partition=mulan,main
 #SBATCH --time=3:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=64G
-#SBATCH --output=/net/mulan/home/fredboe/research/simple-CVplus-sims/cluster_outputs/cvp.out
-#SBATCH --error=/net/mulan/home/fredboe/research/simple-CVplus-sims/cluster_outputs/cvp.err
+#SBATCH --output=/net/mulan/home/fredboe/research/simple-CVplus-sims/cluster_outputs/simple2.out
+#SBATCH --error=/net/mulan/home/fredboe/research/simple-CVplus-sims/cluster_outputs/simple2.err
 
 conda run -n barber1 quarto render simple2.qmd --to html
 
